7f5f0eca07b9e916d2b0baee5bf1aaf14a3579a5
Cibichakkaravarthi R <52355794+Cibi-R@users.noreply.github.com>
2022-03-20 13:50:52 +0530

    updated added flashing info to Flash console

--- ST_Serial/windows/Flash.cs
+++ ST_Serial/windows/Flash.cs
@@ -9,10 +9,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace comm_tool.windows
 {
     public partial class Flash : Form
     {
+
+        public partial class FlashInfo
+        {
+            public int TotalRecord;
+            public int FlashedRecords;
+            public int RemainingRecords;
+
+            public FlashInfo()
+            {
+                this.TotalRecord = 0;
+                this.FlashedRecords = 0;
+                this.RemainingRecords = 0;
+            }
+        };
+
+        FlashInfo info = new FlashInfo();
+        System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+
         /*< collection of hex records are stored in this queue */
         private List<List<byte>> HexFileList = new List<List<byte>>();
 
@@ -86,6 +105,10 @@ namespace comm_tool.windows
                     HexFileList.Add(frame);
                 }
 
+                info.TotalRecord = HexFileList.Count;
+                info.FlashedRecords = 0;
+                stopwatch.Start();
+
                 richTextBox1.Text += "> flashing started..." + Environment.NewLine;
 
                 backgroundWorker1.RunWorkerAsync();
@@ -96,6 +119,11 @@ namespace comm_tool.windows
         {
             bool response_received = false;
 
+            if (!code.serialport.IsPortOpen())
+            {
+                code.serialport.SerialPort_Open();
+            }
+
             foreach (List<byte> record in HexFileList)
             {
                 response_received = false;
@@ -103,6 +131,8 @@ namespace comm_tool.windows
                 if (!code.serialport.SerialPort_WriteByteArray(record.ToArray()))
                 {
                     /*< transmitting frame failed */
+                    e.Cancel = true;
+
                     break;
                 }
                 else
@@ -115,6 +145,8 @@ namespace comm_tool.windows
                         {
                             if (code.serialport.SerialPort_ReadByte()[1] == 0X01)
                             {
+                                info.FlashedRecords++;
+
                                 response_received = true;
                             }
                             else
@@ -138,6 +170,8 @@ namespace comm_tool.windows
 
         private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            stopwatch.Stop();
+
             if (e.Cancelled)
             {
                 richTextBox1.Text += "> reflashing failed due to nack/no response" + Environment.NewLine;
@@ -146,6 +180,15 @@ namespace comm_tool.windows
             {
                 richTextBox1.Text += "> reflashing succefully completed..." + Environment.NewLine;
             }
+
+            info.RemainingRecords = info.TotalRecord - info.TotalRecord;
+
+            richTextBox1.Text += "> Total records    : " + info.TotalRecord.ToString() + Environment.NewLine;
+            richTextBox1.Text += "> Flashed records  : " + info.FlashedRecords.ToString() + Environment.NewLine;
+            richTextBox1.Text += "> Remaining records: " + info.RemainingRecords.ToString() + Environment.NewLine;
+            richTextBox1.Text += "> Time Elapsed     : " + stopwatch.ElapsedMilliseconds.ToString() + "ms" + Environment.NewLine;
+
+            stopwatch.Reset();
         }
     }
 }
